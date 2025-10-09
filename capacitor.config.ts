432b23a9d6cebc54f12986c0ec38f4d14a73ac2f
ndengeyintwali <ndengeyintwalifils@gmail.com>
2025-10-09 01:33:20 -0800

    Remove capacitor.config.ts and sync Android app

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vocabmaster.app',
-  appName: 'VocabMaster',
-  webDir: 'build'
-};
-
-export default config;
